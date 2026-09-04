10e6cde2bb62c8cd04763ba9eea9cb4c0c33fa4c
nodirbekz <iceone843@gmail.com>
2026-09-05 00:20:52 +0500

    fix(db): update placeholder string again with query params to satisfy neon regex

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,6 +2,6 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
-const dbUrl = process.env.DATABASE_URL || 'postgresql://user:password@example.com/dbname';
+const dbUrl = process.env.DATABASE_URL || 'postgresql://user:password@host.tld/dbname?sslmode=require';
 const sql = neon(dbUrl);
 export const db = drizzle(sql, { schema });
